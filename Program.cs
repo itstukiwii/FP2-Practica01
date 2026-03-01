77df89f21758209df6ad62275823c33881a2fd79
Tuki <martreye@ucm.es>
2026-03-01 23:17:51 +0100

    Juego base funcional, falta comentar mueveBloque

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 // (Hermes)
 // Practica 1: Unblock Me
 
+using System;
+
 namespace FP2_Practica01
 {
     internal class Program
@@ -25,8 +27,32 @@ namespace FP2_Practica01
             Console.WriteLine("Introduce el numero de nivel que quieres jugar:");
             int n = int.Parse(Console.ReadLine());
             Estado e = LeeNivel("levels.txt", n); // se lee el nivel del archivo y se guarda en un estado
-            Render(e); // se renderiza el estado inicial
 
+            bool fin = false; // indica si se ha ganado o no
+            bool win = false; // indica si se ha ganado o no, se muestra un mensaje al ganar
+            char tecla;       // tecla pulsada por el jugador
+
+            Render(e); // se renderiza el estado inicial
+            while (!fin)
+            {
+                tecla = LeeInput(); // se lee la tecla pulsada por el jugador
+                if (tecla == 'q') fin = true; // si se pulsa q, se sale del juego
+                else
+                {
+                    ProcesaInput(ref e, tecla); // se procesa la tecla pulsada y se actualiza el estado
+                    Render(e);                  // se renderiza el nuevo estado
+                    if (e.mat[e.sal.x, e.sal.y] == e.obj) // se comprueba si el bloque objetivo ha llegado a la salida
+                    {
+                        fin = true; // se ha ganado, se sale del bucle
+                        win = true; // se ha ganado, se muestra el mensaje de victoria
+                    }
+                }
+            }
+            if (win)
+            {
+                Console.WriteLine(); // se deja hueco entre el tablero y el mensaje de victoria
+                Console.WriteLine("¡Has ganado! Yayy L(^w^)/"); // mensaje de victoria :3
+            }
 
 
 
@@ -128,6 +154,7 @@ namespace FP2_Practica01
             }
         }
 
+
         static void Render(Estado est)
         {
             Console.Clear(); 
@@ -146,9 +173,12 @@ namespace FP2_Practica01
                 }
             }
             Console.SetCursorPosition(est.act.x * 2, est.act.y); // se daja espacio entre columnas para que se vea mejor visualmente
-            Console.ForegroundColor = BloqueToInt(est.mat[est.act.x, est.act.y], colores);
+            Console.BackgroundColor = BloqueToInt(est.mat[est.act.x, est.act.y], colores); // se deja el fondo del color que estaba para que mole más
+            Console.ForegroundColor= ConsoleColor.White;
             if (est.sel) Console.Write("**"); // se pinta el cursor de seleccionado
             else Console.Write("<>");         // o el cursor de no seleccionado
+            Console.BackgroundColor= ConsoleColor.Black; // lo reseteo
+
             Console.SetCursorPosition(2, est.mat.GetLength(1) + 1); // se pinta el objetivo, dejando espacio respecto al muro (por eso el +1)
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Objetivo: ");
@@ -158,6 +188,7 @@ namespace FP2_Practica01
         }
         static ConsoleColor BloqueToInt(char c, ConsoleColor[] color) // se accede directamente al enum, no solo al número
         { 
+            if (c == '.') return color[0]; // el espacio libre se pinta, para evitar excepciones al mover el cursor por un espacio libre
             return color[(int)c - (int)'a' + 1]; // a = 1, b = 2, etc, el +1 es para que el bloque a no se corresponda con el color negro que es el de los espacios
         }
         /*static int BloqueToInt(char c)
@@ -165,6 +196,133 @@ namespace FP2_Practica01
             return ((int)c) - ((int)'a') + 1; // a = 1, b = 2, etc
         }*/
 
+
+        static void MueveCursor(ref Estado est, Coor dir)
+        {
+            if (est.act.x - 1 == 0 && dir.x < 0) dir.x = 0;                        // evita salirse del tablero por la izquierda
+            if (est.act.x + 1 == est.mat.GetLength(0) - 1 && dir.x > 0) dir.x = 0; // evita salirse del tablero por la derecha
+            if (est.act.y + 1 == est.mat.GetLength(1) - 1 && dir.y > 0) dir.y = 0; // evita salirse del tablero por abajo
+            if (est.act.y - 1 == 0 && dir.y < 0) dir.y = 0;                        // evita salirse del tablero por arriba
+            est.act.x = est.act.x + dir.x; // se actualiza la posición del cursor
+            est.act.y = est.act.y + dir.y; // lo mismo de arriba ||
+        }
+        static void MueveBloque(ref Estado est, Coor dir)
+        {
+            Coor[] blok;
+            char c = est.mat[est.act.x, est.act.y];
+            blok = new Coor[AUXILIO(est, c)];
+            EncuentraPorfa(est, blok, c);
+
+            if (est.mat[blok[0].x - 1, est.act.y] != '.' && dir.x < 0) dir.x = 0;
+            if (est.mat[blok[blok.Length - 1].x + 1, est.act.y] != '.' && dir.x > 0) dir.x = 0;
+            if (est.mat[est.act.x, blok[0].y - 1] != '.' && dir.y < 0) dir.y = 0;
+            if (est.mat[est.act.x, blok[blok.Length - 1].y + 1] != '.' && dir.y > 0) dir.y = 0;
+            if (est.mat[est.act.x + 1, est.act.y] == c || est.mat[est.act.x - 1, est.act.y] == c)
+            {
+                if (dir.x < 0 && dir.y == 0) 
+                { 
+                    est.mat[blok[0].x + dir.x, est.act.y] = c; 
+                    est.mat[blok[blok.Length - 1].x, est.act.y] = '.'; 
+                    est.act.x = est.act.x + dir.x;
+                }
+                else if (dir.x > 0 && dir.y == 0) 
+                {
+                    est.mat[blok[blok.Length - 1].x + dir.x, est.act.y] = c;
+                    est.mat[blok[0].x, est.act.y] = '.'; 
+                    est.act.x = est.act.x + dir.x; 
+                }
+            }
+            else if (est.mat[est.act.x, est.act.y + 1] == c || est.mat[est.act.x, est.act.y - 1] == c)
+            {
+                if (dir.y < 0 && dir.x == 0) 
+                {
+                    est.mat[est.act.x, blok[0].y + dir.y] = c;
+                    est.mat[est.act.x, blok[blok.Length - 1].y] = '.';
+                    est.act.y = est.act.y + dir.y; 
+                }
+                else if (dir.y > 0 && dir.x == 0) 
+                { 
+                    est.mat[est.act.x, blok[blok.Length - 1].y + dir.y] = c;
+                    est.mat[est.act.x, blok[0].y] = '.';
+                    est.act.y = est.act.y + dir.y; 
+                }
+            }
+        }
+        static int AUXILIO(Estado est, char c) //mide la barrita en la que esta el cursor, cuando la está seleccionando, creo
+        {
+            int aux = 0;
+            if (est.mat[est.act.x + 1, est.act.y] == c || est.mat[est.act.x - 1, est.act.y] == c)
+            {
+                for (int i = 0; i < est.mat.GetLength(0); i++)
+                {
+                    if (est.mat[i, est.act.y] == c) aux++;
+                }
+            }
+            else if (est.mat[est.act.x, est.act.y + 1] == c || est.mat[est.act.x, est.act.y - 1] == c)
+            {
+                for (int i = 0; i < est.mat.GetLength(1); i++)
+                {
+                    if (est.mat[est.act.x, i] == c) aux++;
+                }
+            }
+            return aux;
+        }
+        static void EncuentraPorfa(Estado est, Coor[] n, char c) //este si funciona lo juro coño
+        {
+            int aux = 0;
+            if (est.mat[est.act.x + 1, est.act.y] == c || est.mat[est.act.x - 1, est.act.y] == c)
+            {
+                for (int i = 0; i < est.mat.GetLength(0); i++)
+                {
+                    if (est.mat[i, est.act.y] == c) { n[aux].x = i; n[aux].y = est.act.y; aux++; }
+                }
+            }
+            else if (est.mat[est.act.x, est.act.y + 1] == c || est.mat[est.act.x, est.act.y - 1] == c)
+            {
+                for (int i = 0; i < est.mat.GetLength(1); i++)
+                {
+                    if (est.mat[est.act.x, i] == c) { n[aux].x = est.act.x; n[aux].y = i; aux++; }
+                }
+            }
+        }
+
+
+        static void ProcesaInput(ref Estado est, char c)
+        {
+            Coor dir;
+            dir.x = 0; dir.y = 0; // se inicializa la dirección a 0,0 para luego actualizarla según la tecla pulsada
+
+            if (c == 's')
+            {
+                if (est.mat[est.act.x, est.act.y] != '.') est.sel = !est.sel; 
+                // si se pulsa s, cambia el estado al contrario (solo si no está en un espacio libre)
+            }
+            switch (c)
+            {
+                // si está seleccionado, se mueve el bloque en la dirección indicada
+                // si no está seleccionado, se mueve el cursor en la dirección indicada
+                case 'u': 
+                    dir.y = -1; dir.x = 0; 
+                    if (est.sel) MueveBloque(ref est, dir); 
+                    else MueveCursor(ref est, dir);
+                    break;
+                case 'd': 
+                    dir.y = 1; dir.x = 0; 
+                    if (est.sel) MueveBloque(ref est, dir); 
+                    else MueveCursor(ref est, dir);
+                    break;
+                case 'r': 
+                    dir.y = 0; dir.x = 1; 
+                    if (est.sel) MueveBloque(ref est, dir); 
+                    else MueveCursor(ref est, dir);
+                    break;
+                case 'l': 
+                    dir.y = 0; dir.x = -1; 
+                    if (est.sel) MueveBloque(ref est, dir); 
+                    else MueveCursor(ref est, dir);
+                    break;
+            }
+        }
         static char LeeInput()
         {
             char d = ' ';
